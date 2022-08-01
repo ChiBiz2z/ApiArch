282d645da381db7d39a9f096e7aa15a299bad416
Vladimir Bedash <vvbedash@gmail.com>
2022-08-01 18:14:32 +0600

    Создание сущности сотрудников с атрибутами для mongo db

--- src/EmployeesAPI/Models/Employee.cs
+++ src/EmployeesAPI/Models/Employee.cs
@@ -1,10 +1,17 @@
-﻿namespace EmployeesAPI.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace EmployeesAPI.Models
 {
     public class Employee
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public int Id { get; set; }
+        [BsonElement("Name")]
         public string Name { get; set; }
+        [BsonElement("Surname")]
         public string Surname { get; set; }
+        [BsonElement("Age")]
         public int Age { get; set; }
     }
 }
