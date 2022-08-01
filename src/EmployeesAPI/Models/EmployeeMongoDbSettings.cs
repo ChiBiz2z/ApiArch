0cb4ee58b882bc599785bc8ed4d0f4cde6000cec
Vladimir Bedash <vvbedash@gmail.com>
2022-08-01 20:14:21 +0600

    Налажена связь с базой данных путём создания класса настроек и вызова нужных методов свойства Services объекта builder.
    Изменен файл appsettings.json для удобного соединения с бд.

---
+++ src/EmployeesAPI/Models/EmployeeMongoDbSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeesAPI.Models
+{
+    public class EmployeeMongoDbSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string EmployeesCollectionName { get; set; } = null!;
+    }
+}
--- src/EmployeesAPI/Program.cs
+++ src/EmployeesAPI/Program.cs
@@ -1,13 +1,23 @@
+using EmployeesAPI.Models;
+using EmployeesAPI.Services;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.Configure<EmployeeMongoDbSettings>(
+    builder.Configuration.GetSection("EmployeeMongoDb"));
+
+builder.Services.AddSingleton<EmployeeService>();
+
+builder.Services.AddControllers()
+    .AddJsonOptions(
+        options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -34,6 +44,7 @@ app.MapGet("/weatherforecast", () =>
     return forecast;
 })
 .WithName("GetWeatherForecast");
+
 
 app.Run();
 
