1df4b19a1a258aa7005339d5246e926e977f96e4
Vladimir Bedash <vvbedash@gmail.com>
2022-08-02 14:53:03 +0600

    Добавлены методы для группировки запросов на странице Swagger'a

--- src/EmployeesAPI/Program.cs
+++ src/EmployeesAPI/Program.cs
@@ -2,7 +2,6 @@ using EmployeesAPI.Interfaces;
 using EmployeesAPI.Models;
 using EmployeesAPI.Services;
 using Microsoft.Extensions.Options;
-using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,52 +27,52 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//Member
 app.MapGet("/api/organization/{organizationId}/members",
     (MemberService service, string organizationId) =>
         service.GetByOrganization(organizationId)
-);
+).WithTags("Members");
 
 app.MapGet("/members",
     async (MemberService service) =>
         await service.GetAsync()
-);
+).WithTags("Members");
 
 app.MapGet("/members/{id}",
     async (MemberService service, string id) =>
-        await service.GetById(id));
+        await service.GetById(id)).WithTags("Members");
 
 app.MapDelete("/members/remove/{id}",
     async (MemberService service, string id) =>
-        await service.RemoveAsync(id));
+        await service.RemoveAsync(id)).WithTags("Members");
 
 app.MapPost("/members/add/",
     async (MemberService service, Member member) =>
-        await service.CreateAsync(member));
+        await service.CreateAsync(member)).WithTags("Members");
 
 app.MapPut("/members/update/{id}",
     async (MemberService service, string id, Member member) =>
-        await service.UpdateAsync(id, member));
+        await service.UpdateAsync(id, member)).WithTags("Members");
 
-//Organizaion
-
+//Organization
 app.MapGet("/organizations",
     async (OrganizationService service) =>
-        await service.GetAsync());
+        await service.GetAsync()).WithTags("Organization");
 
 app.MapGet("/organizations/{id}",
     async (OrganizationService service, string id) =>
-        await service.GetById(id));
+        await service.GetById(id)).WithTags("Organization");
 
 app.MapDelete("/organizations/remove/{id}",
     async (OrganizationService service, string id) =>
-        await service.RemoveAsync(id));
+        await service.RemoveAsync(id)).WithTags("Organization");
 
 app.MapPost("/organizations/add/",
     async (OrganizationService service, Organization organization) =>
-        await service.CreateAsync(organization));
+        await service.CreateAsync(organization)).WithTags("Organization");
 
 app.MapPut("/organizations/update/{id}",
     async (OrganizationService service, string id, Organization organization) =>
-        await service.UpdateAsync(id, organization));
+        await service.UpdateAsync(id, organization)).WithTags("Organization");
 
 app.Run();
