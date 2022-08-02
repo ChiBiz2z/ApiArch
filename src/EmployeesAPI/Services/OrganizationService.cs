a7c6698506bee5cee159f38d2926cf24677d4633
Vladimir Bedash <vvbedash@gmail.com>
2022-08-02 13:43:48 +0600

    Добавлены методы по получению по Id, созданию новой, удалению и обновлению организации.

--- src/EmployeesAPI/Services/OrganizationService.cs
+++ src/EmployeesAPI/Services/OrganizationService.cs
@@ -25,5 +25,17 @@ namespace EmployeesAPI.Services
         public async Task<List<Organization>> GetAsync() =>
             await _organizationCollection.Find(_ => true).ToListAsync();
 
+        //TODO СДЕЛАТЬ ПРОВЕРКУ НА NULL И ВОЗВРАЩАТЬ BADREQUEST'ы т.п
+        public async Task<Organization> GetById(string id) =>
+            await _organizationCollection.Find(o => o.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Organization newOrganization) =>
+            await _organizationCollection.InsertOneAsync(newOrganization);
+
+        public async Task RemoveAsync(string id) =>
+            await _organizationCollection.DeleteOneAsync(o => o.Id == id);
+
+        public async Task UpdateAsync(string id, Organization newOrganization) =>
+            await _organizationCollection.ReplaceOneAsync(o => o.Id == id, newOrganization);
     }
 }
