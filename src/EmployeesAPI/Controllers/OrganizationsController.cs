7148b4823e2c5531612d128a0f5f91fda39398d8
Vladimir Bedash <vvbedash@gmail.com>
2022-08-02 10:43:17 +0600

    Добавлен контроллер организации с экземеляром класса OrganizationService

---
+++ src/EmployeesAPI/Controllers/OrganizationsController.cs
@@ -0,0 +1,28 @@
+﻿using EmployeesAPI.Models;
+using EmployeesAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrganizationsController : ControllerBase
+    {
+        private readonly OrganizationService _organizationService;
+        private readonly MemberService _memberService;
+
+        public OrganizationsController(OrganizationService organizationService) =>
+            _organizationService = organizationService;
+
+        [HttpGet]
+        public async Task<List<Organization>> Get()
+        {
+            return await _organizationService.GetAsync();
+        }
+
+
+        [HttpGet("{organizationId}")]
+        public async Task<List<Member>> GetMembers(string id) =>
+            await _memberService.GetByOrganization(id);
+    }
+}
