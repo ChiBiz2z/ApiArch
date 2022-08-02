c58ec34a656362f4d1ad0921e6b7dd663d52322d
Vladimir Bedash <vvbedash@gmail.com>
2022-08-02 13:45:04 +0600

    Созданы методы-запросы для CRUD операций над сотрудниками и организациями

--- src/EmployeesAPI/Program.cs
+++ src/EmployeesAPI/Program.cs
@@ -2,6 +2,7 @@ using EmployeesAPI.Interfaces;
 using EmployeesAPI.Models;
 using EmployeesAPI.Services;
 using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,8 +38,42 @@ app.MapGet("/members",
         await service.GetAsync()
 );
 
+app.MapGet("/members/{id}",
+    async (MemberService service, string id) =>
+        await service.GetById(id));
+
+app.MapDelete("/members/remove/{id}",
+    async (MemberService service, string id) =>
+        await service.RemoveAsync(id));
+
+app.MapPost("/members/add/",
+    async (MemberService service, Member member) =>
+        await service.CreateAsync(member));
+
+app.MapPut("/members/update/{id}",
+    async (MemberService service, string id, Member member) =>
+        await service.UpdateAsync(id, member));
+
+//Organizaion
+
 app.MapGet("/organizations",
     async (OrganizationService service) =>
         await service.GetAsync());
+
+app.MapGet("/organizations/{id}",
+    async (OrganizationService service, string id) =>
+        await service.GetById(id));
+
+app.MapDelete("/organizations/remove/{id}",
+    async (OrganizationService service, string id) =>
+        await service.RemoveAsync(id));
+
+app.MapPost("/organizations/add/",
+    async (OrganizationService service, Organization organization) =>
+        await service.CreateAsync(organization));
+
+app.MapPut("/organizations/update/{id}",
+    async (OrganizationService service, string id, Organization organization) =>
+        await service.UpdateAsync(id, organization));
 
 app.Run();
