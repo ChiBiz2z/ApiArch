a200d2d004bb02332625e3e7af401f6d91e33c1c
Vladimir Bedash <vvbedash@gmail.com>
2022-08-02 10:46:24 +0600

    Создан сервис отвечающий за взаимодействие с сотрудниками.

---
+++ src/EmployeesAPI/Services/MemberService.cs
@@ -0,0 +1,29 @@
+﻿using EmployeesAPI.Interfaces;
+using EmployeesAPI.Models;
+using MongoDB.Driver;
+
+namespace EmployeesAPI.Services
+{
+    public class MemberService
+    {
+        private readonly IMongoCollection<Member> _memberCollection;
+
+        public MemberService(IEmployeeMongoDbSettings settings)
+        {
+            var mongoClient = new MongoClient(
+                settings.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                settings.DatabaseName);
+
+            _memberCollection = mongoDatabase.GetCollection<Member>(
+                settings.MembersCollectionName);
+        }
+
+        public async Task<List<Member>> GetAsync() =>
+            await _memberCollection.Find(_ => true).ToListAsync();
+
+        public async Task<List<Member>> GetByOrganization(string id)
+            => await _memberCollection.Find(_ => _.OrganizationId == id).ToListAsync();
+    }
+}
