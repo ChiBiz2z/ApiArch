7ac11806b3037255fa4d60fa39409b2de8ef32be
Vladimir Bedash <vvbedash@gmail.com>
2022-08-03 11:09:48 +0600

    Добавлена сущность Member

---
+++ src/EmployeesAPI.Domain/Member.cs
@@ -0,0 +1,51 @@
+﻿namespace EmployeesAPI.Domain
+{
+    public class Member
+    {
+        public string Key { get; }
+        public string Name { get; }
+        public string Surname { get; }
+        public int Age { get; }
+        public string OrganizationKey { get; } //Id или Key?
+
+        public Member(string key, string name, string surname, int age, string organizationKey)
+        {
+            Validator(name, organizationKey);
+            
+            Key = key;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            OrganizationKey = organizationKey;
+        }
+
+        public Member(string key, string name, string organizationKey)
+        {
+            Validator(name, organizationKey);
+
+            Key = key;
+            Name = name;
+            OrganizationKey = organizationKey;
+        }
+
+        public Member(string name, string surname, int age, string organizationKey)
+        {
+            Validator(name, organizationKey);
+            
+            Name = name;
+            Surname = surname;
+            Age = age;
+            OrganizationKey = organizationKey;
+            Key = Guid.NewGuid().ToString();
+        }
+
+        private void Validator(string name, string key)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("Имя сотрудника не валидно");
+
+            if (!Guid.TryParse(key, out _))
+                throw new ArgumentNullException("Id организации сотрудника не валиден");
+        }
+    }
+}
