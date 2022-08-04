3b2618218869d111831ac6e19dbada72f96ad2a0
Vladimir Bedash <vvbedash@gmail.com>
2022-08-04 09:20:33 +0600

    #4 Перенос Маппинга
    Создан класс MemberEndPoints.cs.
    
    Внутри создан метод расширения для WebApplication, куда были перенесены все Map методы, связанные с сотрудниками.

---
+++ src/EmployeesAPI/Members/MemberEndPoints.cs
@@ -0,0 +1,49 @@
+﻿using EmployeesAPI.Members.MemberRequests;
+
+namespace EmployeesAPI.Members;
+
+public static class MemberEndPoints
+{
+    public static WebApplication? MapMemberEndpoints(this WebApplication? app, string prefix)
+    {
+        if (app == null)
+            return app;
+
+        app.MapDelete("/members/{id}",
+            async (MemberService service, string id) =>
+            {
+                var request = new DeleteMemberRequest
+                {
+                    Key = id
+                };
+
+                return await service.RemoveAsync(request);
+            }).WithTags("Members");
+
+
+        app.MapGet("/members/{id}",
+            async (MemberService service, string id) =>
+            {
+                var request = new GetMemberRequest
+                {
+                    Key = id
+                };
+
+                return await service.GetById(request);
+            }).WithTags("Members");
+
+        app.MapPost("/members/",
+            async (MemberService service, CreateMemberRequest request) =>
+                await service.CreateAsync(request)).WithTags("Members");
+
+        app.MapPut("/members/{id}",
+            async (MemberService service, string id, UpdateMemberRequest request) =>
+            {
+                request.Key = id;
+                await service.UpdateAsync(request);
+            }).WithTags("Members");
+
+
+        return app;
+    }
+}
--- src/EmployeesAPI/Program.cs
+++ src/EmployeesAPI/Program.cs
@@ -26,56 +26,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//Member
-// app.MapGet("/api/organization/{organizationId}/members",
-//     (MemberService service, string organizationId) =>
-//         service.GetByOrganization(organizationId)
-// ).WithTags("Members");
-//
-// app.MapGet("/members",
-//     async (MemberService service) =>
-//         await service.GetAsync()
-// ).WithTags("Members");
-
-//
-app.MapDelete("/members/{id}",
-    async (MemberService service, string id) =>
-    {
-        var request = new DeleteMemberRequest
-        {
-            Key = id
-        };
-
-        return await service.RemoveAsync(request);
-    }).WithTags("Members");
-
-
-app.MapGet("/members/{id}",
-    async (MemberService service, string id) =>
-    {
-        var request = new GetMemberRequest
-        {
-            Key = id
-        };
-
-        return await service.GetById(request);
-    }).WithTags("Members");
-
-app.MapPost("/members/",
-    async (MemberService service, CreateMemberRequest request) =>
-        await service.CreateAsync(request)).WithTags("Members");
-
-app.MapPut("/members/{id}",
-    async (MemberService service, string id, UpdateMemberRequest request) =>
-    {
-        request.Key = id;
-        await service.UpdateAsync(request);
-    }).WithTags("Members");
-
-
 var basePrefix = "/api";
 var organizationPrefix = "/organizations";
-
 app.MapOrganizationEndpoints($"{basePrefix}{organizationPrefix}");
+app.MapMemberEndpoints($"{basePrefix}");
 
 app.Run();
