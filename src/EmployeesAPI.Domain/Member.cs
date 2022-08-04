2f689447dc84cafa3971557349f3f7a87aa9997f
Vladimir Bedash <vvbedash@gmail.com>
2022-08-04 12:30:48 +0600

    #5 Добавлен проект для тестов EmployeeAPI.Tests
    Созданы:
    - Тест на проверку возраста Member'a
    - Тест на проверку имени и ключей Member'a
    - Тест на проверу имени и ключа организации
    - Тест на невозможность создания организации с пустым именем.

--- src/EmployeesAPI.Domain/Member.cs
+++ src/EmployeesAPI.Domain/Member.cs
@@ -5,12 +5,13 @@
         public string Key { get; }
         public string Name { get; }
         public string Surname { get; }
+        public string Fullname => $"{Name} {Surname}";
         public int Age { get; }
         public string OrganizationKey { get; }
 
         public Member(string key, string name, string surname, int age, string organizationKey)
         {
-            Validator(name, organizationKey);
+            Validator(name, organizationKey, age);
 
             Key = key;
             Name = name;
@@ -24,13 +25,16 @@
         {
         }
 
-        private void Validator(string name, string key)
+        private void Validator(string name, string key, int age)
         {
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentNullException("Имя сотрудника не валидно");
 
             if (!Guid.TryParse(key, out _))
                 throw new ArgumentNullException("Id организации сотрудника не валиден");
+
+            if (age < 16 || age > 99)
+                throw new ArgumentOutOfRangeException("Возраст должен быть больше 16 и меньше 99");
         }
     }
 }
